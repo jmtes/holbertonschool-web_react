be35cc3540fa941daba7a49d993fcb8570b28f50
jmtes <juliennetesoro@gmail.com>
2020-08-10 18:22:51 -0700

    Define StudentClass class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -22,3 +22,32 @@ const printTeacher: printTeacherFunction = (
   const firstInitial: string = firstName.charAt(0);
   return `${firstInitial}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
