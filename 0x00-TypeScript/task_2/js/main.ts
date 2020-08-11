6b8a7baf6c69092a6f36b9da90df74f39ab229ca
jmtes <juliennetesoro@gmail.com>
2020-08-10 19:00:33 -0700

    Define teachClass function

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -55,3 +55,10 @@ const executeWork = (employee: Director | Teacher): string => {
   if (isDirector(employee)) return employee.workDirectorTasks();
   else return employee.workTeacherTasks();
 };
+
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === 'Math') return 'Teaching Math';
+  if (todayClass === 'History') return 'Teaching History';
+};
