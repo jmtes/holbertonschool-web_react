b3c704cbd296d75337f956b73e685240312d75bf
jmtes <juliennetesoro@gmail.com>
2020-08-17 13:54:10 -0700

    Create Notifications component

--- 0x02-react_intro/task_1/src/index.js
+++ 0x02-react_intro/task_1/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Notifications from './dashboard/Notifications';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Notifications />
   </React.StrictMode>,
   document.getElementById('root')
 );
