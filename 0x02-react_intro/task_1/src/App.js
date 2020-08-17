a9121c80790aabd37f767580f4e17ee9ea50fc65
jmtes <juliennetesoro@gmail.com>
2020-08-17 13:37:47 -0700

    Copy code from previous task

---
+++ 0x02-react_intro/task_1/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import logo from './dashboard/logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className='App-header'>
+        <img src={logo} alt='Logo'></img>
+        <h1>School Dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Log in to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
