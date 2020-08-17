ea6f2e181e428286236608052fef5348850c7389
jmtes <juliennetesoro@gmail.com>
2020-08-17 15:21:16 -0700

    Add styles

--- 0x02-react_intro/task_2/src/App.js
+++ 0x02-react_intro/task_2/src/App.js
@@ -13,10 +13,10 @@ function App() {
       </div>
       <div className='App-body'>
         <p>Log in to access the full dashboard</p>
-        <label htmlFor='email'>Email</label>
+        <label htmlFor='email'>Email:</label>
         <input type='email' name='email' id='email' />
 
-        <label htmlFor='password'>Password</label>
+        <label htmlFor='password'>Password:</label>
         <input type='password' name='password' id='password' />
 
         <button>OK</button>
