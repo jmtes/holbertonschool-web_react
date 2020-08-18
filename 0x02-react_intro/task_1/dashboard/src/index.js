fcc6fee013242021f178895771dee3b82f445e62
jmtes <juliennetesoro@gmail.com>
2020-08-17 19:24:32 -0700

    Fix file structure

--- 0x02-react_intro/task_1/dashboard/src/index.js
+++ 0x02-react_intro/task_1/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Notifications from './dashboard/Notifications';
+import Notifications from './Notifications';
 
 ReactDOM.render(
   <React.StrictMode>
