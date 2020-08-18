24483239ed3e1ddcbd920e8cd068ef84682555ad
jmtes <juliennetesoro@gmail.com>
2020-08-17 23:52:21 -0700

    Configure Webpack

---
+++ 0x02-react_intro/task_5/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'public/index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
+  }
+};
