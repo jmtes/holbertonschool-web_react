660aadcd081fb75ac964319090f1eb2a1fd458bc
jmtes <juliennetesoro@gmail.com>
2020-08-17 18:42:41 -0700

    Create test suite

--- 0x02-react_intro/task_3/src/dashboard/utils.test.js
+++ 0x02-react_intro/task_3/src/dashboard/utils.test.js
@@ -1,19 +1,21 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-test('getFullYear returns correct year', () => {
-  expect(getFullYear()).toBe(new Date().getFullYear());
-});
+describe('utils', () => {
+  test('getFullYear returns correct year', () => {
+    expect(getFullYear()).toBe(new Date().getFullYear());
+  });
 
-test('getFooterCopy returns correct string if isIndex is true', () => {
-  expect(getFooterCopy(true)).toBe('Holberton School');
-});
+  test('getFooterCopy returns correct string if isIndex is true', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School');
+  });
 
-test('getFooterCopy returns correct string id isIndex is false', () => {
-  expect(getFooterCopy(false)).toBe('Holberton School Main Dashboard');
-});
+  test('getFooterCopy returns correct string id isIndex is false', () => {
+    expect(getFooterCopy(false)).toBe('Holberton School Main Dashboard');
+  });
 
-test('getLatestNotification returns correct string', () => {
-  expect(getLatestNotification()).toBe(
-    '<strong>Urgent requirement</strong> - complete by EOD'
-  );
+  test('getLatestNotification returns correct string', () => {
+    expect(getLatestNotification()).toBe(
+      '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+  });
 });
