bbb0abcbb8dc38157a050883860acc442335aeb9
jmtes <juliennetesoro@gmail.com>
2020-08-22 15:35:35 -0700

    Replace list items with NotificationItems

--- 0x0a_react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x0a_react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -4,17 +4,19 @@ import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
 import { getLatestNotification } from '../utils/utils';
 
+import NotificationItem from './NotificationItem';
+
 export const Notification = () => {
   return (
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority='default'>New course available</li>
-        <li data-priority='urgent'>New resume available</li>
-        <li
-          data-priority='urgent'
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
+        <NotificationItem type='default' value='New course available' />
+        <NotificationItem type='urgent' value='New resume available' />
+        <NotificationItem
+          type='urgent'
+          html={{ __html: getLatestNotification() }}
+        />
       </ul>
       <button
         style={{
