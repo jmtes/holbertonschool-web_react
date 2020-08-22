aa3363b53207a6a5735f64ada247efd4f9527f1e
jmtes <juliennetesoro@gmail.com>
2020-08-22 14:04:11 -0700

    Remove tests

--- 0x0a_react_props/task_0/dashboard/src/App/App.test.js
+++ 0x0a_react_props/task_0/dashboard/src/App/App.test.js
@@ -8,25 +8,4 @@ describe('App', () => {
 
     expect(wrapper.exists()).toBe(true);
   });
-
-  test('renders header div', () => {
-    const wrapper = shallow(<App />);
-    const header = wrapper.find('div.App-header');
-
-    expect(header.exists()).toBe(true);
-  });
-
-  test('renders body div', () => {
-    const wrapper = shallow(<App />);
-    const body = wrapper.find('div.App-body');
-
-    expect(body.exists()).toBe(true);
-  });
-
-  test('renders footer div', () => {
-    const wrapper = shallow(<App />);
-    const footer = wrapper.find('div.App-footer');
-
-    expect(footer.exists()).toBe(true);
-  });
 });
