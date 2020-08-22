95eb5ad53ac522234ee36f37756b3a4ef39f63b8
jmtes <juliennetesoro@gmail.com>
2020-08-22 14:03:36 -0700

    Move import

--- 0x0a_react_props/task_0/dashboard/src/App/App.js
+++ 0x0a_react_props/task_0/dashboard/src/App/App.js
@@ -5,15 +5,19 @@ import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
 
 function App() {
   return (
     <Fragment>
-      <Header />
-      <div className='App-body'>
-        <Login />
+      <Notifications />
+      <div className='App'>
+        <Header />
+        <div className='App-body'>
+          <Login />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </Fragment>
   );
 }
