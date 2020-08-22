13c3a8dd59b1a4f4ee109355730fb69120bb456c
jmtes <juliennetesoro@gmail.com>
2020-08-22 15:55:48 -0700

    Modify and add tests

--- 0x0a_react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x0a_react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 
 import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
 describe('Notifications', () => {
   test('renders without crashing', () => {
@@ -12,7 +13,7 @@ describe('Notifications', () => {
 
   test('renders three list items', () => {
     const wrapper = shallow(<Notifications />);
-    const listItems = wrapper.find('li');
+    const listItems = wrapper.find(NotificationItem);
 
     expect(listItems.length).toBe(3);
   });
@@ -22,5 +23,17 @@ describe('Notifications', () => {
     const text = wrapper.find('p');
 
     expect(text.text()).toBe('Here is the list of notifications');
+  });
+
+  test('NotificationItem renders correct html', () => {
+    const wrapper = shallow(<Notifications />);
+
+    const firstNotifItem = wrapper.find(NotificationItem).at(0);
+
+    expect(firstNotifItem.props()).toHaveProperty('type', 'default');
+    expect(firstNotifItem.props()).toHaveProperty(
+      'value',
+      'New course available'
+    );
   });
 });
