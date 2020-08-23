d29ca56d4d8728c3ea7dd14dde9e05acff3eb738
jmtes <juliennetesoro@gmail.com>
2020-08-22 18:18:25 -0700

    Check prop types and add default props

--- 0x0a_react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x0a_react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const NotificationItem = ({ type, html, value }) => {
   if (html === undefined) return <li data-notification-type={type}>{value}</li>;
@@ -6,6 +7,16 @@ const NotificationItem = ({ type, html, value }) => {
     return (
       <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
     );
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  value: PropTypes.string
+};
+
+NotificationItem.defaultProps = {
+  type: 'default'
 };
 
 export default NotificationItem;
