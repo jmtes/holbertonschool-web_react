e972b570d402bfc9b1ca142046a2b413a53c145a
jmtes <juliennetesoro@gmail.com>
2020-08-22 18:06:23 -0700

    Show either login form or course list

--- 0x0a_react_props/task_4/dashboard/src/App/App.js
+++ 0x0a_react_props/task_4/dashboard/src/App/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 import './App.css';
 
@@ -6,20 +7,30 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
 
-function App() {
+function App({ isLoggedIn }) {
   return (
     <Fragment>
       <Notifications />
       <div className='App'>
         <Header />
         <div className='App-body'>
-          <Login />
+          {!isLoggedIn && <Login />}
+          {isLoggedIn && <CourseList />}
         </div>
         <Footer />
       </div>
     </Fragment>
   );
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+App.defaultProps = {
+  isLoggedIn: false
+};
 
 export default App;
