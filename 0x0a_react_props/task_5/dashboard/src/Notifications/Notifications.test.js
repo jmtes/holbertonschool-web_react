d8e94e76d63f4a92f1c0999a38b85f0ac071d2ca
jmtes <juliennetesoro@gmail.com>
2020-08-23 12:32:16 -0700

    Modify tests

--- 0x0a_react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x0a_react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -12,35 +12,6 @@ describe('Notifications', () => {
   });
 
   describe('displayDrawer is true', () => {
-    // OLD TESTS START
-    test('renders three list items', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true} />);
-      const listItems = wrapper.find(NotificationItem);
-
-      expect(listItems.length).toBe(3);
-    });
-
-    test('renders the correct text', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true} />);
-      const text = wrapper.find('p');
-
-      expect(text.text()).toBe('Here is the list of notifications');
-    });
-
-    test('NotificationItem renders correct html', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true} />);
-
-      const firstNotifItem = wrapper.find(NotificationItem).at(0);
-
-      expect(firstNotifItem.props()).toHaveProperty('type', 'default');
-      expect(firstNotifItem.props()).toHaveProperty(
-        'value',
-        'New course available'
-      );
-    });
-
-    // OLD TESTS END
-
     test('menu item is displayed', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
       const menuItem = wrapper.find('.menuItem');
@@ -70,5 +41,64 @@ describe('Notifications', () => {
 
       expect(notifs.length).toBe(0);
     });
+  });
+
+  describe('listNotifications is empty', () => {
+    test('renders correctly if empty array is passed', () => {
+      const wrapper = shallow(<Notifications />);
+      const notifs = wrapper.find(NotificationItem);
+
+      expect(notifs.length).toBe(0);
+    });
+
+    test('renders correctly if listNotifications prop not specified', () => {
+      const wrapper = shallow(<Notifications />);
+      const notifs = wrapper.find(NotificationItem);
+
+      expect(notifs.length).toBe(0);
+    });
+  });
+
+  describe('listNotifications is not empty', () => {
+    const testNotifs = [
+      { id: 0, type: 'default', value: 'New project available' },
+      {
+        id: 1,
+        type: 'urgent',
+        html: { __html: '<strong>Project Deadline Approaching</strong>' }
+      }
+    ];
+
+    const wrapper = shallow(
+      <Notifications listNotifications={testNotifs} displayDrawer={true} />
+    );
+    const notifs = wrapper.find(NotificationItem);
+
+    test('renders correct amount of notifications', () => {
+      expect(notifs.length).toBe(2);
+    });
+
+    test('renders notifications with correct attributes', () => {
+      const notif0 = notifs.at(0);
+      const notif1 = notifs.at(1);
+
+      expect(notif0.props()).toHaveProperty('type', 'default');
+      expect(notif0.props()).toHaveProperty('value', 'New project available');
+      expect(notif0.props()).toHaveProperty('html', undefined);
+
+      expect(notif1.props()).toHaveProperty('type', 'urgent');
+      expect(notif1.props()).toHaveProperty('value', '');
+      expect(notif1.props()).toHaveProperty('html', {
+        __html: '<strong>Project Deadline Approaching</strong>'
+      });
+    });
+  });
+
+  describe('message displays properly', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+    const noNewNotifs = wrapper.find('.Notifications p');
+
+    expect(noNewNotifs.length).toBe(1);
+    expect(noNewNotifs.text()).toBe('No new notifications for now');
   });
 });
