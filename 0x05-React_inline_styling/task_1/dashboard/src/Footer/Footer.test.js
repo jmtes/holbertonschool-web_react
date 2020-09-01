ee9d50b9e52807db16b1044ba0f6cc5a62a45712
jmtes <juliennetesoro@gmail.com>
2020-09-01 16:02:53 -0700

    Suppress style injections

--- 0x05-React_inline_styling/task_1/dashboard/src/Footer/Footer.test.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Footer/Footer.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 import Footer from './Footer';
 
 describe('Footer', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   test('renders without crashing', () => {
     const wrapper = shallow(<Footer />);
 
