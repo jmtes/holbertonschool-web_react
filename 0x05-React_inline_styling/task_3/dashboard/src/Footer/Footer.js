c93bec64c12d1b86c97860381d6825a16c860264
jmtes <juliennetesoro@gmail.com>
2020-09-01 16:45:37 -0700

    Only fix footer on large screens

--- 0x05-React_inline_styling/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-React_inline_styling/task_3/dashboard/src/Footer/Footer.js
@@ -14,13 +14,16 @@ const Footer = () => {
 const styles = StyleSheet.create({
   footer: {
     width: '100%',
-    position: 'fixed',
     bottom: '0px',
     borderTop: '3px solid #e1354b',
     textAlign: 'center',
     fontStyle: 'italic',
     padding: '1rem 0',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    position: 'fixed',
+    '@media (max-width: 900px)': {
+      position: 'static'
+    }
   }
 });
 
