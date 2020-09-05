5da26b7835e2d169c36427d650d40a2eb7608075
jmtes <juliennetesoro@gmail.com>
2020-09-05 13:55:08 -0700

    Import app context and implement logout

--- 0x06-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x06-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { StyleSheet, css } from 'aphrodite';
+
+import AppContext from '../App/AppContext';
 
 import logo from '../assets/logo.jpg';
 
-const Header = () => {
-  return (
-    <div className={css(styles.header)}>
-      <img src={logo} alt='Logo' className={css(styles.logo)}></img>
-      <h1 className={css(styles.heading)}>School Dashboard</h1>
-    </div>
-  );
-};
+class Header extends Component {
+  render() {
+    const { user, logOut } = this.context;
+
+    return (
+      <Fragment>
+        <div className={css(styles.header)}>
+          <img src={logo} alt='Logo' className={css(styles.logo)}></img>
+          <h1 className={css(styles.heading)}>School Dashboard</h1>
+        </div>
+        {user.isLoggedIn && (
+          <div id='logoutSection'>
+            <p>
+              Welcome <strong>{user.email} </strong>
+              <em onClick={logOut} className={css(styles.logout)}>
+                (logout)
+              </em>
+            </p>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   header: {
@@ -28,7 +46,12 @@ const styles = StyleSheet.create({
     display: 'inline',
     position: 'relative',
     color: '#e1345b'
+  },
+  logout: {
+    cursor: 'pointer'
   }
 });
+
+Header.contextType = AppContext;
 
 export default Header;
