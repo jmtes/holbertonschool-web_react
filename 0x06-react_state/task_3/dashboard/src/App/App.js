22aa231b8c77372c317795bb78c8a37786c20727
jmtes <juliennetesoro@gmail.com>
2020-09-05 17:24:37 -0700

    Move listNotifications to state

--- 0x06-react_state/task_3/dashboard/src/App/App.js
+++ 0x06-react_state/task_3/dashboard/src/App/App.js
@@ -24,7 +24,18 @@ class App extends Component {
         this.setState({
           ...this.state,
           user: { email: '', password: '', isLoggedIn: false }
-        })
+        }),
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        {
+          id: 3,
+          type: 'urgent',
+          html: {
+            __html: '<strong>Urgent requirement</strong> - complete by EOD'
+          }
+        }
+      ]
     };
   }
 
@@ -72,24 +83,12 @@ class App extends Component {
 
   render() {
     const { isLoggedIn } = this.state.user;
-    const { user, logOut } = this.state;
+    const { user, logOut, listNotifications } = this.state;
 
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 }
-    ];
-
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      {
-        id: 3,
-        type: 'urgent',
-        html: {
-          __html: '<strong>Urgent requirement</strong> - complete by EOD'
-        }
-      }
     ];
 
     return (
