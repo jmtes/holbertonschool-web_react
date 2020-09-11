7f7d2b3e133d73d2a2a647d518d0d2c958c5ff27
jmtes <juliennetesoro@gmail.com>
2020-09-11 13:09:09 -0700

    Deeply merge objects

---
+++ 0x07-React_Immutable/6-deeply.js
@@ -0,0 +1,12 @@
+import immutable from 'immutable';
+
+const { Map } = immutable;
+
+const mergeDeeplyElements = (page1, page2) => {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  return map1.mergeDeep(map2);
+};
+
+export default mergeDeeplyElements;
