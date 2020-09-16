43aa07672943d5d741a62bdfc53225c2b9f01ef3
jmtes <juliennetesoro@gmail.com>
2020-09-16 14:40:50 -0700

    Change test suite name

--- 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/actions/courseActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/actions/courseActionCreators.test.js
@@ -1,7 +1,7 @@
 import { selectCourse, unSelectCourse } from './courseActionCreators';
 import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
 
-describe('action creators return expected values', () => {
+describe('course action creators return expected values', () => {
   test('selectCourse', () => {
     // Call with index of 1
     const ret = selectCourse(1);
