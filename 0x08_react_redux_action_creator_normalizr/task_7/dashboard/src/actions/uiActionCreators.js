0617a06ac13aaac944b7fc25f2ef997464ebba27
jmtes <juliennetesoro@gmail.com>
2020-09-17 11:51:51 -0700

    Add more action creators

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,9 +1,13 @@
 import {
   LOGIN,
   LOGOUT,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER
 } from './uiActionTypes';
+
+import apiResponse from '../../dist/login-success.json';
 
 export const login = (email, password) =>
   dispatch({
@@ -11,7 +15,15 @@ export const login = (email, password) =>
     user: { email, password }
   });
 
+export const loginRequest = (email, password) => {
+  login(email, password);
+};
+
 export const logout = () => dispatch({ type: LOGOUT });
+
+export const loginSuccess = () => dispatch({ type: LOGIN_SUCCESS });
+
+export const loginFailure = () => dispatch({ type: LOGIN_FAILURE });
 
 export const displayNotificationDrawer = () =>
   dispatch({
